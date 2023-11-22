5cd6c7f0c10c0ef2dc292b64e39a2f3accd743fc
Tyuiu.GoogeRA@outlook.com <Tyuiu.GoogeRA@outlook.com>
2023-11-22 19:56:05 +0300

    Task5

---
+++ Tyuiu.GoogeRA.Sprint5.Task5.V27.Lib/DataService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+//Найти среднее значение всех целых чисел в файле, которые делятся на 5. 
+namespace Tyuiu.GoogeRA.Sprint5.Task5.V27.Lib
+{
+    public class DataService : ISprint5Task5V27
+    {
+        public double LoadFromDataFile(string path)
+        {
+            int res = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+     
+                while ((line = reader.ReadLine()) != null )
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (line[i] % 5 == 0) i++;
+                        {
+                            res++;
+                            
+                        }
+                    }
+                }
+            }
+            return res;
+
+            //int num = int.Parse(Console.ReadLine());
+            //while (num != 0)
+            //{
+             ///   if (num % 5 = 0) k++;
+                
+            //}
+            //Console.WriteLine($"Количество чисел кратных пяти = ", k);
+
+
+        }
+    }
+}
---
+++ Tyuiu.GoogeRA.Sprint5.Task5.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GoogeRA.Sprint5.Task5.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GoogeRA.Sprint5.Task5.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d1af67e4-a466-4300-817a-ac2f09f6603a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
