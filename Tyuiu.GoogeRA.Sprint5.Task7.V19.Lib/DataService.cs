2482f16e70b890511a5b4f0d461626a8e5a69798
Tyuiu.GoogeRA@outlook.com <Tyuiu.GoogeRA@outlook.com>
2023-11-25 16:58:24 +0300

    Task7

---
+++ Tyuiu.GoogeRA.Sprint5.Task7.V19.Lib/DataService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+//Удалить все удвоенные буквы "сс" из файла.Полученный результат сохранить в файл OutPutDataFileTask7V19.txt.
+namespace Tyuiu.GoogeRA.Sprint5.Task7.V19.Lib
+{
+    public class DataService : ISprint5Task7V19
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = @"C:\DataSprint5\OutPutDataFileTask7V19.txt";
+
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(pathSaveFile); 
+            }
+
+            string strLine = "";
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (line[i] != 'с' && i != line.Length - 1 && line[i + 1] != 'с')
+                        {
+                            strLine = strLine + line[i];
+                        }
+                    }
+                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
+                    strLine = "";
+                }
+
+            }
+            return pathSaveFile;
+
+        }
+    }
+}
