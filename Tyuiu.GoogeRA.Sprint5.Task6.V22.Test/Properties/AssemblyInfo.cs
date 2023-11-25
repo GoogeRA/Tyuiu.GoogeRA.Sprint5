25b1c7399d91f46cc215280552f9906ac5282691
Tyuiu.GoogeRA@outlook.com <Tyuiu.GoogeRA@outlook.com>
2023-11-25 15:39:48 +0300

    Task6

---
+++ Tyuiu.GoogeRA.Sprint5.Task6.V22.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GoogeRA.Sprint5.Task6.V22.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GoogeRA.Sprint5.Task6.V22.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cd037c15-3390-4fab-9419-7d0aa4699e77")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
